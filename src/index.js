eb865b88b583d4535bf13c492d746a8bcc4676b0
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-19 14:17:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
+  fs.writeFile('myfile.txt', 'Newton School', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
   // write code here
   // dont change function name
 };
