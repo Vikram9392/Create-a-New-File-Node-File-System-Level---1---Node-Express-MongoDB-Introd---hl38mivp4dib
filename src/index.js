960ecf2b0bdf8a6b2f8d6ae3694a98b23072c794
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-19 14:28:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
-  try { 
-     await writeFile(fileName, fileContent); 
-     console.log(`Wrote data to ${fileName}`); 
-   } catch (error) { 
-     console.error(`Got an error trying to write the file: ${error.message}`); 
-   } 
+ 
   // write code here
   // dont change function name
 };
